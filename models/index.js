4958bfc9174ad87ac1fd34bd058d600b997bc2a1
KhoaMinHi <66743540+khoaminhi@users.noreply.github.com>
2022-08-05 08:08:44 +0700

    remove sensitive data

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 
 const initModels = require('./init-models');
 //const connPostgresString = process.env.DB_URL;//add require('dotenv').config(); into www
-const connPostgresString = 'postgres://wxjfftihybqpun:584926c1c528115d979e1e2cd8ac86a41acc3da6b44eaefdb5be61662790a3de@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d75gjnh8aca4q1';
+const connPostgresString = 'postgres://username:password@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d75gjnh8aca4q1';
 const sequelize = new Sequelize(connPostgresString, {
     dialect: 'postgres',
     dialectOptions: {
